0ff2f01ee7b861a6ce757b0ca4421a648a410004
LeoKuhorev <kuhorev.leonid@gmail.com>
2019-11-20 16:16:52 -0800

    initial server setup

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-const express = require('express');
+'use strict';
+// Basic server setup
+// Load Environment veriable from the .env
+require('dotenv').config();
 
+// Declare Application Dependencies
+const express = require('express');
+const superagent = require('superagent');
+const cors = require('cors');
+const path = require('path');
+
+// Application setup
 const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.set('view engine', 'ejs');
+
+
+// Routes
+// Serving static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  
+  res.status(200).render('pages/index');
 });
 
-const PORT = process.env.PORT || 3000;
+app.get('*', (req, res) => {
+  res.status(200).render('pages/error');
+});
 
+// Ensure that the server is listening for requests
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
